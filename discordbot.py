67740f15b9ca0ba967e580480cfebd407c181510
森若 直也 <bcz030952@gintra.bc-glex.net>
2020-01-16 12:29:35 +0900

    決定版

--- discordbot.py
+++ discordbot.py
@@ -6,35 +6,43 @@ import re
 # さいころの和を計算する用の関数
 from func import  diceroll
 
-bot = commands.Bot(command_prefix='!')
+#bot = commands.Bot(command_prefix='/')
+bit = discord.Cliant()
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@cliant.event
+async def on_ready():
+    print('--------------')
+    print('ログインしました')
+    print(client.user.name)
+    print(client.user.id)
+    print('--------------')
+    channel = client.get_channel('チャンネルID')
+    await channel.send('ダイスロールは任せろ―！ばりばり～')
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@bot.command()
-async def dice(ctx, msg):
-    m = re.match(r"^\d+d\d+$", msg.lower())
+@client.event
+async def on_message(msg):
+    #bot空の発言は無視
+    if message.author.bot:
+        return
+    #オーダーを含む発言にマッチするか
+    m = re.match(r"^!d(?:ice)?\s(\d+d\d+)$", message.content.lower())
     if m:
-        await  ctx.send(msg)    
+        await  msg.channel.send(msg)
     else:
-        await  ctx.send("Invalid value. Write like NdM. (N and M are integers)")
+        await  msg.channel.send("Invalid value. Write like MdN. (M and N are integers)")
         return
 
-    cnt, mx = list(map(int, msg.split('d'))) # さいころの個数と面数
-    dice = diceroll(cnt, mx) # 和を計算する関数(後述)
-        # サイコロの目の総和を表示
-    await  ctx.send(sum(dice))
-        # さいころの目の内訳を表示する
-    await  ctx.send(dice)
+    # 入力された内容を受け取る
+    order = m.group(1)  #dice を振る回数
+    mx = m.group(2)   #dice の出目
 
-bot.run(token)
+    if order >= 100 :
+        msg.channel.send("Order value:M is invalid. (Valid values are 1-100.)")
+        return 
+
+    resalt = diceroll(order, mx) # m面ダイスをn回振る関数
+    await message.channel.send(sum(resalt)) # さいころの総和を表示
+    await message.channel.send(dice)        # さいころの目の内訳を表示する
+
+client.run(token)
